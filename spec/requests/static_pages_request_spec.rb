83e04f8dd9c7dd2c6a66d2ae20ccd724f5028962
nittamatama <blackmnshin@gmail.com>
2022-04-12 08:06:43 +0900

    abouttest

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -4,14 +4,21 @@ RSpec.describe "StaticPages", type: :request do
 
   describe "GET /home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get static_pages_home_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /help" do
     it "returns http success" do
-      get "/static_pages/help"
+      get static_pages_help_path
+      expect(response).to have_http_status :ok
+    end
+  end
+
+  describe "GET /about" do
+    it "returns http success" do
+      get static_pages_about_path
       expect(response).to have_http_status(:success)
     end
   end
