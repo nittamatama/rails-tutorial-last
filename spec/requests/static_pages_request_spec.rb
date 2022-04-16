8e7f833ac93781df6f4a894bd4ae7b2b73d054c3
nittamatama <blackmnshin@gmail.com>
2022-04-16 16:10:12 +0900

    provideを追加

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -7,12 +7,22 @@ RSpec.describe "StaticPages", type: :request do
       get static_pages_home_path
       expect(response).to have_http_status(:success)
     end
+
+    it "titleにHome | Ruby on Rails Tutorial Sample Appが表示される" do
+      get static_pages_home_path
+      expect(response.body).to include "Home | Ruby on Rails Tutorial Sample App"
+    end
   end
 
   describe "GET /help" do
     it "returns http success" do
       get static_pages_help_path
-      expect(response).to have_http_status :ok
+      expect(response).to have_http_status(:success)
+    end
+
+    it "titleにHelp | Ruby on Rails Tutorial Sample Appが表示される" do
+      get static_pages_help_path
+      expect(response.body).to include "Help | Ruby on Rails Tutorial Sample App"
     end
   end
 
@@ -20,6 +30,11 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get static_pages_about_path
       expect(response).to have_http_status(:success)
+    end
+
+    it "titleにAbout | Ruby on Rails Tutorial Sample Appが表示される" do
+      get static_pages_about_path
+      expect(response.body).to include "About | Ruby on Rails Tutorial Sample App"
     end
   end
 
