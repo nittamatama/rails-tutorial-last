086f4797efa24a892ab878a11d630663e011e2f0
nittamatama <blackmnshin@gmail.com>
2022-04-25 20:29:35 +0900

    validattion

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: 'Example User', email: 'user@example.com') }
+
+  it 'userが有効であること' do
+    expect(user).to be_valid
+  end
+
+  it '名前が存在しないといけない' do
+    user.name = '     '
+    expect(user).to_not be_valid
+  end
+
+  it 'メールアドレスが存在しないといけない' do
+    user.email = '     '
+    expect(user).to_not be_valid
+  end
 end
