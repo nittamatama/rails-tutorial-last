48b3f21100be95e6821f0502ab4d2f9bb2af5f6e
nittamatama <blackmnshin@gmail.com>
2022-05-02 15:38:33 +0900

    ぼっちだったり

--- spec/system/users_logins_spec.rb
+++ spec/system/users_logins_spec.rb
@@ -6,6 +6,22 @@ RSpec.describe "UsersLogins", type: :system do
   end
 
   describe "#new" do
+    context "有効な値の場合" do
+      let(:user) { FactoryBot.create(:user) }
+
+      it "ログインユーザー用ページが表示される" do
+        visit login_path
+
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Log in"
+
+        expect(page).to_not have_selector "a[href=\"#{login_path}\"]"
+        expect(page).to have_selector "a[href=\"#{logout_path}\"]"
+        expect(page).to have_selector "a[href=\"#{user_path(user)}\"]"
+      end
+    end
+
     context "無効な値の場合" do
       it "flashメッセージが表示される" do
         visit login_path
